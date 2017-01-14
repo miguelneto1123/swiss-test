738ee245ce1bfeec8afccbe5642ce1d1333c5179
Miguel <miguelneto.1995@hotmail.com>
2017-01-14 16:04:43 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SwissTest
+=========
+
+A Symfony project created on January 14, 2017, 3:57 pm.
---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miguel
+ * Date: 14/01/17
+ * Time: 16:00
+ */
+
+namespace AppBundle\Controller;
+
+
+class LoginController
+{
+
+}
